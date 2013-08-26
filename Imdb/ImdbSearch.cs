794d488d80172f07c3a24505f1d0bb48cf3962ff
rajasanjeevkumar <raja@ksu.edu>
2013-08-26 18:28:52 +0530

    imdb search - json object complete

---
+++ Imdb/ImdbSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Microsoft.JScript;
+
+namespace imdbSearch
+{
+    public class ImdbSearch
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool movieFound(string movieName)
+        {
+            WebClient client = new WebClient();
+
+            string result = client.DownloadString("http://www.omdbapi.com/?i=&t=" + movieName);
+            ImdbMovie movie = Newtonsoft.Json.JsonConvert.DeserializeObject<ImdbMovie>(result);
+            if (movie.Title == null)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    public class ImdbMovie
+    {
+        public string Title { get; set; }
+        public decimal imdbRating { get; set; }
+        public string imdbVotes { get; set; }
+        public long ImdbVotes { get { return long.Parse(imdbVotes, System.Globalization.NumberStyles.Any); } }
+    }
+}
---
+++ Imdb/ImdbSearchTest.cs
@@ -0,0 +1,109 @@
+﻿using imdbSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ImdbSearchTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ImdbSearchTest and is intended
+    ///to contain all ImdbSearchTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ImdbSearchTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for single word movieFound
+        ///</summary>
+        [TestMethod()]
+        public void movieSingleWordTest()
+        {
+            ImdbSearch target = new ImdbSearch(); // TODO: Initialize to an appropriate value
+            string movieName = "Memento"; // TODO: Initialize to an appropriate value
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.movieFound(movieName);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for multiple words movieFound
+        ///</summary>
+        [TestMethod()]
+        public void movieMultipleWordsTest()
+        {
+            ImdbSearch target = new ImdbSearch(); // TODO: Initialize to an appropriate value
+            string movieName = "The Lord of the rings"; // TODO: Initialize to an appropriate value
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.movieFound(movieName);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for single word movieFound
+        ///</summary>
+        [TestMethod()]
+        public void movieWrongTextTest()
+        {
+            ImdbSearch target = new ImdbSearch(); // TODO: Initialize to an appropriate value
+            string movieName = "REWQ"; // TODO: Initialize to an appropriate value
+            bool expected = false; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.movieFound(movieName);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
