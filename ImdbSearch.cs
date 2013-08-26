3db29085d8ddada53a09e65ecc6c1f34b16e519a
rajasanjeevkumar <raja@ksu.edu>
2013-08-26 17:41:07 +0530

    imdb search - omdbapi - json object - class not yet created

---
+++ ImdbSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace imdbSearch
+{
+    public class ImdbSearch
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool movieFound(string movieName)
+        {
+            WebClient client = new WebClient();
+
+            string result = client.DownloadString("http://www.omdbapi.com/?i=&t=" + movieName);
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            Test routes_list =
+                   (Test)json_serializer.DeserializeObject("{ \"test\":\"some data\" }");
+            return false;
+        }
+    }
+}
